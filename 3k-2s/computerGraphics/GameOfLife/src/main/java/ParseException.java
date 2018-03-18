da1e8675330d7a0643257cb41c0c3bfcd27c6909
andreysher <andreysher97@gmail.com>
2018-03-18 14:19:18 +0700

    graphics update

--- 3k-2s/computerGraphics/GameOfLife/src/main/java/ParseException.java
+++
@@ -1,9 +0,0 @@
-public class ParseException extends Exception{
-    String reason;
-    public ParseException(String reason){
-        this.reason = reason;
-    }
-    public void printMessage(){
-        System.out.println(reason);
-    }
-}
